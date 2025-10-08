6d28667b3775c89751649b4467d9458bdc153250
DIMAS ID <dimas.kurnia829@smk.belajar.id>
2025-10-08 20:52:56 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -58,18 +58,15 @@ export default function Home() {
               HTML Developer and Designer web
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              HTML Developer &amp; Designer crafting clean code, modern visuals, and meaningful user experiences.
-            </p>
-            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              join me down below and Let&apos;s get cracking.
-            </p>
+  HTML Developer &amp; Designer crafting clean code, modern visuals, and meaningful user experiences. join me down below and Let&apos;s get cracking.
+</p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               <AiFillTwitterCircle aria-label="Twitter" />
               <AiFillLinkedin aria-label="LinkedIn" />
               <AiFillYoutube aria-label="YouTube" />
             </div>
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
-              <Image src={deved} layout="fill" objectFit="cover" alt="Profile avatar" />
+              <Image src={deved} layout="fill" objectFit="cover" alt="Profile avatar"/>
             </div>
           </div>
         </section>
@@ -77,7 +74,7 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white">Services I offer</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey a freelance designer and developer, I&apos;ve done remote work for
+              Since the beginning of my journey a freelance designer and developer, done remote work for
               <span className="text-teal-500"> agencies </span>
               consulted for <span className="text-teal-500">startups </span>
               and collaborated with talented people to create digital products for both business and consumer use.</p>
