336df2f752759499f62c24cf09ee3e81834c6078
DIMAS ID <dimas.kurnia829@smk.belajar.id>
2025-10-08 20:28:42 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,18 +74,16 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white">Services I offer</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and developer, I&apos;ve done remote work for
+              Since the beginning of my journey a freelance designer and developer, I&apos;ve done remote work for
               <span className="text-teal-500"> agencies </span>
               consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talented people to create digital products for both business and consumer use.
-            </p>
+              and collaborated with talented people to create digital products for both business and consumer use.</p>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design, programming and teaching.
-            </p>
+              I offer from a wide range of services, including brand design, programming and teaching.</p>
           </div>
           <div className="lg:flex gap-10">
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={design} width={100} height={100} alt="Design illustration" />
+              <Image src={design} width={100} height={100} alt="Design illustration"/>
               <h3 className="text-lg font-medium pt-8 pb-2">
                 Beautiful Designs
               </h3>
@@ -104,8 +102,7 @@ export default function Home() {
                 Code your dream project
               </h3>
               <p className="py-2">
-                Do you have an idea for your next great website? Let&apos;s make it a reality.
-              </p>
+                Do you have an idea for your next great website? Let&apos;s make it a reality.</p>
               <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
               <p className="text-gray-800 py-1">Photoshop</p>
               <p className="text-gray-800 py-1">Illustrator</p>
